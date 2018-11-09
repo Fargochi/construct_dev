0e98fd64ec32b9f1d600c2992def7e238c8eb225
Ari*.h <fargochih@gmail.com>
2018-11-09 14:08:46 +0300

    express

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.listen(3000)
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'John'
+    })
+})
