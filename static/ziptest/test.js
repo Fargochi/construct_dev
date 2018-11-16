0ff7624723fa481e631d5e3f932dff22e92a4b4d
Suchenochka <ananas-pod-kaifom@yandex.ru>
2018-11-16 10:06:49 +0300

    Написана функция next(), отвечающая за вывод нового вопроса ученику

--- static/ziptest/test.js
+++ static/ziptest/test.js
@@ -1,5 +1,37 @@
 var test[]; //заполняется из данных пользователя скриптом
+var number_question = 0;
 start()
+{   
+	document.getElementById('base').innerHTML = " ";
+	button_next = '<button onClick="next();">Следующий вопрос</button>';
+	document.getElementById('nextquestion').innerHTML = button_next;
+	setquestion();
+}
+next()
+{ 
+	chek();
+	setquestion();
+}
+chek()
 {
 
+
 }
+setquestion()
+{
+	//работа c div "question"
+	var newdivquestion = '<div class="question'+ number_question + '">'+ test[number_question].question +'</div>"'
+	document.getElementById('question').innerHTML = newdivquestion;
+	//работа с div "answers"
+	document.getElementById('answers').innerHTML = " ";
+	var divanswers = document.getElementById("answers"); 
+	var n = test[number_question].answers.length;
+	var i;
+	for (i=0;i<n;i++)
+	{
+		newanswer = '<div class = "answer'+ i + '">'+ test[number_question].answers[i].text + '</div>'; //создания div конкретного вопроса. НАДО ДОБАВИТЬ ЧЕКЕР
+		divanswers.innerHTML += newanswer; //добавление этого div
+	}
+
+}
+
